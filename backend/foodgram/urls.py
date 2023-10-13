f4f1cb1066a163822c329778eac87c92e8288314
octrow <octrow@yandex.ru>
2023-10-13 18:40:38 +0600

    v.1.5.5

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,5 +1,5 @@
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,8 +7,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+#
+# if settings.DEBUG:
+#     urlpatterns += static(
+#         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+#     )
